155a15fb26e45d9097ea156c0be0ec521fd9968a
vahid <vahid.mardani@gmail.com>
2015-07-19 19:49:53 +0430

    2.0.0 -alpha ! some methods renamed, new formatter/parser engine fully tested.

--- tests/test_teh_tz.py
+++ tests/test_teh_tz.py
@@ -11,19 +11,14 @@ class TestTehTz(unittest.TestCase):
         self.dt = datetime(2000, 1, 1)
         self.dtz = datetime(2000, 1, 1, tzinfo=self.tz)
 
-#    def first_test(self):
-#        n = datetime.now()
-#        n.astimezone(self.tz)
-        
     def test_dst(self):
         naive_time = JalaliDatetime(1390, 12, 29, 1, 1, 1, 1)
-        nodst_time = JalaliDatetime(1390, 12, 29, 1, 1, 1, 1, tzinfo=self.tz)
-        dst_time = nodst_time + timedelta(5)
+        dst_aware_time = JalaliDatetime(1390, 12, 29, 1, 1, 1, 1, tzinfo=self.tz)
+        dst_time = dst_aware_time + timedelta(5)
         self.assertIsNotNone(dst_time.dst(), "invalid dst")
-        self.assertEqual(nodst_time.dst(), timedelta(0), "invalid dst")
+        self.assertEqual(dst_aware_time.dst(), timedelta(0), "invalid dst")
         self.assertIsNone(naive_time.dst(), "invalid dst")
 
-        
-    
+
 if __name__ == '__main__':
     unittest.main()        
