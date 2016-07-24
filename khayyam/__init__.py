20f09adf04c4022a134a1e12ba690a46e71a13ee
vahid <vahid@dobisel.com>
2016-07-24 17:17:21 +0430

    version 2.10.0

--- khayyam/__init__.py
+++ khayyam/__init__.py
@@ -4,7 +4,7 @@ from .jalali_date import JalaliDate
 from .jalali_datetime import JalaliDatetime
 from .timezones import TehranTimezone, Timezone
 
-__version__ = '2.9.8'
+__version__ = '2.10.0'
 
 
 teh_tz = TehranTimezone()
