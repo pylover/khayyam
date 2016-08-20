ee88c331b5764d3ba179b5d5bb0a9ebbe08efb8e
vahid <vahid@dobisel.com>
2016-08-20 17:34:27 +0430

    Travis deploy docs

--- travis-gh-pages.sh
+++ travis-gh-pages.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+set -e # Exit with nonzero exit code if anything fails
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SHA=`git rev-parse --verify HEAD`
+
 
 # Clone/checkout the gh-pages branch from Github alongside the master branch working copy directory :
 cd ..
@@ -13,6 +19,29 @@ make khayyam.io
 
 # Commit & push
 cd ../../khayyam.io/
-git commit -am "From travis #$TRAVIS_JOB_ID"
-git push origin gh-pages
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
 
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+git add .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+
+git push origin gh-pages
