00f90c3d5f3987fa436a1c4e50bee9641ece536b
vahid <vahid@dobisel.com>
2016-08-20 18:03:14 +0430

    Travis gh pages

--- travis-gh-pages.sh
+++ travis-gh-pages.sh
@@ -5,6 +5,20 @@ set -e # Exit with nonzero exit code if anything fails
 REPO=`git config remote.origin.url`
 SHA=`git rev-parse --verify HEAD`
 
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+OUT_KEY="../../.ssh/khayyam-travis-gh-pages"
+#openssl aes-256-cbc -K $encrypted_d39830e202a7_key -iv $encrypted_d39830e202a7_iv -in khayyam-travis-gh-pages.enc -out ../../.ssh/khayyam-travis-gh-pages -d
+
+
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in khayyam-travis-gh-pages.enc -out $OUT_KEY -d
+chmod 600 $OUT_KEY
+eval `ssh-agent -s`
+ssh-add $OUT_KEY
+
 
 # Clone/checkout the gh-pages branch from Github alongside the master branch working copy directory :
 cd ..
@@ -31,20 +45,4 @@ fi
 
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-OUT_KEY="../../.ssh/khayyam-travis-gh-pages"
-#openssl aes-256-cbc -K $encrypted_d39830e202a7_key -iv $encrypted_d39830e202a7_iv -in khayyam-travis-gh-pages.enc -out ../../.ssh/khayyam-travis-gh-pages -d
-
-
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in khayyam-travis-gh-pages.enc -out $OUT_KEY -d
-chmod 600 $OUT_KEY
-eval `ssh-agent -s`
-ssh-add $OUT_KEY
-
 git push origin gh-pages
