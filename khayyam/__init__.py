b369ce45d24dc8432c85b9a9f15f150b8a680f51
vahid <vahid@dobisel.com>
2016-08-25 15:26:26 +0430

    Imorting JalaliDateFormatter & JalaliDatetimeFormatter in __init__.py

--- khayyam/__init__.py
+++ khayyam/__init__.py
@@ -3,6 +3,7 @@ from .constants import *
 from .jalali_date import JalaliDate
 from .jalali_datetime import JalaliDatetime
 from .timezones import TehranTimezone, Timezone
+from .formatting import JalaliDateFormatter, JalaliDatetimeFormatter
 
 __version__ = '3.0.9'
 
@@ -24,5 +25,7 @@ __all__ = [
     'JalaliDatetime',
     'TehranTimezone',
     'Timezone',
-    'teh_tz'
+    'teh_tz',
+    'JalaliDateFormatter',
+    'JalaliDatetimeFormatter'
 ]
