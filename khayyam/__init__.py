21c8d14110d639d7e67c18f40d62d3ed62cf57fc
vahid <vahid@dobisel.com>
2016-08-27 16:19:22 +0430

    Adding .nojekyll to gh-pages

--- khayyam/__init__.py
+++ khayyam/__init__.py
@@ -5,7 +5,7 @@ from .jalali_datetime import JalaliDatetime
 from .timezones import TehranTimezone, Timezone
 from .formatting import JalaliDateFormatter, JalaliDatetimeFormatter
 
-__version__ = '3.0.11'
+__version__ = '3.0.12'
 
 
 teh_tz = TehranTimezone()
